1b8c91a989e4224050c7a4149477407a8cc8daaf
wail-2002 <wail-2002@hotmail.com>
2026-03-19 19:01:29 +0100

    main

--- src/pizzabar/Main.java
+++ src/pizzabar/Main.java
@@ -7,6 +7,9 @@ public class Main{
         Pizza p1 = new Pizza(5, "Allaorgash", 99);
         System.out.println(p1);
 
+        Pizza p2 = new Pizza(4, "Magherita", 79);
+        System.out.println(p2);
+
         p1.getNummer();
         p1.getNavn();
         p1.getPris();
