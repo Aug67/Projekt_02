43cf1cc632fee04c7166beb0338de094a47b19c9
wail-2002 <wail-2002@hotmail.com>
2026-03-23 18:59:04 +0100

    main scanner somewhat done

--- src/pizzabar/Main.java
+++ src/pizzabar/Main.java
@@ -1,6 +1,6 @@
 package pizzabar;
 
-import java.io.IOException;
+
 import java.util.Scanner;
 
 public class Main{
@@ -8,34 +8,31 @@ public class Main{
     public static void main(String[] args){
 
     Scanner scan = new Scanner(System.in);
-
+int valg = 0;
         System.out.println("\nVælg kundetype:");
         System.out.println("\n1  Normal kunde");
         System.out.println("2  VIP kunde");
         System.out.println("3  Medarbejder kunde");
-            while (true) {
-                try {
-                    int valg = scan.nextInt();
-                    System.out.println("\nVælg kundetype:");
-                    System.out.println("\n1  Normal kunde");
-                    System.out.println("2  VIP kunde");
-                    System.out.println("3  Medarbejder kunde");
+        while (true) {
+            try {
+                valg = scan.nextInt();
 
-                    Customer c;
-                    if (valg == 1) {
-                        c = new NormalCustomer();
-                    } else if (valg == 2) {
-                        c = new VIPCustomer();
-                    } else if (valg == 3) {
-                        c = new EmployeeCustomer();
-                    }
-                } catch (Exception e) {
-                    System.out.println("Skriv et tal imellem 1-3!");
+                if (valg >= 1 && valg <= 3) {
+                    break;
+                } else {
+                    System.out.println("Skriv et tal mellem. 1-3!");
                 }
+
+            } catch (Exception e) {
+                System.out.println("Skriv et tal mellem 1-3!");
+                scan.nextLine();
             }
         }
 
-/*
+    }
+
+
+   /* Customer c;
     if (valg == 1){
         c = new NormalCustomer();
     }  else if (valg == 2){
