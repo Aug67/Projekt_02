57910df60ee2fd46a2dfdc30e328641459977955
Aug67 <zjeberg@gmail.com>
2026-03-23 14:16:59 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/pizzabar/NormalCustomer.java
    #	src/pizzabar/VIPCustomer.java

--- src/pizzabar/Main.java
+++ src/pizzabar/Main.java
@@ -4,16 +4,32 @@ public class Main{
 
     public static void main(String[] args){
 
+
+            // Første bestilling
         Customer c = new VIPCustomer();
         Bestilling b = new Bestilling(c);
 
-
-        b.addPizza(new Pizza(1, "Marghuerita", 80));
+        b.addPizza(new Pizza(1, "Margherita", 80));
         b.addPizza(new Pizza(2, "Pepperoni", 90));
 
-        System.out.println(b.getTotalPris());
+            // Anden bestilling
+        Customer c2 = new NormalCustomer();
+        Bestilling b2 = new Bestilling(c2);
+
+        b2.addPizza(new Pizza(3, "Hawaii", 85));
+
+            // Samling af alle bestillinger
+        AlleBestillinger all = new AlleBestillinger();
+
+        all.addBestilling(b);
+        all.addBestilling(b2);
+
+            // Print alle totalpriser
+        for (Bestilling best : all.getListe()) {
+            System.out.println(best.getTotalPris());
+            }
+        }
 
     }
 
-}
 
