7eed19c28b9591be36a0b8da372c65161789025d
wail-2002 <wail-2002@hotmail.com>
2026-03-24 12:00:14 +0100

    main scanner somewhat done (pizza valg)

--- src/pizzabar/Main.java
+++ src/pizzabar/Main.java
@@ -4,17 +4,21 @@ package pizzabar;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main{
 
-    public static void main(String[] args){
 
-    Scanner scan = new Scanner(System.in);
+public class Main {
 
-int kundeValg = 0;
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int kundeValg = 0;
         System.out.println("\nVælg kundetype:");
         System.out.println("\n1  Normal kunde");
         System.out.println("2  VIP kunde");
         System.out.println("3  Medarbejder kunde");
+
+
         while (true) {
             try {
                 kundeValg = scan.nextInt();
@@ -24,7 +28,7 @@ int kundeValg = 0;
                 } else {
                     System.out.println("Skriv et tal mellem 1-3!");
                 }
-
+                scan.nextLine();
             } catch (Exception e) {
                 System.out.println("Skriv et tal mellem 1-3!");
                 scan.nextLine();
@@ -33,25 +37,51 @@ int kundeValg = 0;
 
         Customer c;
 
-        if (kundeValg == 1){
+        if (kundeValg == 1) {
             c = new NormalCustomer();
-        } else if (kundeValg == 2){
+        } else if (kundeValg == 2) {
             c = new VIPCustomer();
         } else {
             c = new EmployeeCustomer();
         }
 
-        Bestilling b = new Bestilling (c);
+        Bestilling b = new Bestilling(c);
+        Menu menu = new Menu();
 
-        b.addPizza(new Pizza(1, "Margherita", 80));
-        b.addPizza(new Pizza(2, "Pepperoni", 90));
+        menu.addPizza(new Pizza(1, "Margherita", 80));
+        menu.addPizza(new Pizza(2, "Pepperoni", 90));
 
         System.out.println("Bestilling totalpris: " + b.getTotalPris() + " kr");
+        scan.nextLine();
 
-    }
+        ArrayList<Pizza> liste = menu.getPizzaer();
 
-    int pizzaValg = 0;
+        for (Pizza p : liste) {
+            System.out.println(p);
+        }
 
+
+        int pizzaValg = 0;
+
+        boolean fortsaetBestilling = true;
+
+        while (fortsaetBestilling) {
+            try {
+
+                pizzaValg = scan.nextInt();
+                if (pizzaValg == 0) {
+                    fortsaetBestilling = false;
+                    break;
+                } else if (pizzaValg >= 1 && pizzaValg <= 5) {
+                    fortsaetBestilling = true;
+                }
+                scan.nextLine();
+
+            } catch (Exception e) {
+                System.out.println("Skriv et tal mellem 0-5!");
+                scan.nextLine();
+            }
+        }
 
 
    /* Customer c;
@@ -63,12 +93,12 @@ int kundeValg = 0;
         c = new EmployeeCustomer();
     }
 */
-            // Første bestilling
+        // Første bestilling
         //Customer c = new VIPCustomer();
-       // Bestilling b = new Bestilling(c);
+        // Bestilling b = new Bestilling(c);
 
-       // b.addPizza(new Pizza(1, "Margherita", 80));
-       // b.addPizza(new Pizza(2, "Pepperoni", 90));
+        // b.addPizza(new Pizza(1, "Margherita", 80));
+        // b.addPizza(new Pizza(2, "Pepperoni", 90));
 
             /* Anden bestilling
         Customer c2 = new NormalCustomer();
@@ -85,7 +115,8 @@ int kundeValg = 0;
             // Print alle totalpriser
         for (Bestilling best : all.getListe()) {
             System.out.println(best.getTotalPris());*/
-            }
+    }
+}
 
 
 
